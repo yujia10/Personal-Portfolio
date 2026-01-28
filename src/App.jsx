dce05fe10c3dbce693ded0d8c14a63eddc64507a
Yujia Gu <yujiagu1010@gmail.com>
2026-01-28 11:06:16 +1030

    Setup section files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 
+import {Hero} from "@/sections/Hero";
+import {About} from "@/sections/About";
+import {Skills} from "@/sections/Skills";
+import {Projects} from "@/sections/Projects";
+import { Contact } from "./sections/Contact";
+
 function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
 
-
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+    </div>
+  );
 }
 
 export default App
