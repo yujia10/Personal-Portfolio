f0021157c231e581898b8457808a76cc076a4fab
Yujia Gu <yujiagu1010@gmail.com>
2026-01-28 11:19:40 +1030

    Add navbar structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import {Hero} from "@/sections/Hero";
 import {About} from "@/sections/About";
 import {Skills} from "@/sections/Skills";
 import {Projects} from "@/sections/Projects";
-import { Contact } from "./sections/Contact";
+import { Contact } from "@/sections/Contact";
+import { Navbar } from "@/layout/Navbar";
 
 function App() {
   return (
     <div className="min-h-screen overflow-x-hidden">
-
+      <Navbar />
       <main>
         <Hero />
         <About />
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,27 @@
+const navLinks = [
+  {href: "#about", label:"About"},
+  {href: "#skills", label:"Skills"},
+  {href: "#projects", label:"Projects"},
+  {href: "#contat", label:"Contact"},
+]
+
 export const Navbar = () => {
-  return <header></header>
+  return (
+    <header>
+      <nav>
+        <a>
+          YG<span>.</span>
+        </a>
+
+        {/* Desktop Nav */}
+        <div>
+          <div>
+            {navLinks.map((link,index) => (
+              <a href={link.href} key={index}>{link.label}</a>
+            ))}
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
 }
